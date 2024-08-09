32f603af8722f80145c294f1e04c2c8d1f481a97
m1dsolo <yx1053532442@gmail.com>
2024-08-09 21:43:35 +0800

    feat: add speed_potion

--- include/game/buff_manager.hpp
+++ include/game/buff_manager.hpp
@@ -25,54 +25,64 @@ public:
     Buff create(const std::string& name, Args&&... args) {
         auto args_tuple = std::make_tuple(std::forward<Args>(args)...);
         if (name == "hp") {
-            auto [hp] = args_tuple;
+            int hp = std::get<0>(args_tuple);
             return [hp](Entity entity) {
                 ecs.add_components(entity, HealBuffComponent{hp});
             };
         } else if (name == "max_hp") {
-            auto [max_hp] = args_tuple;
+            int max_hp = std::get<0>(args_tuple);
             return [max_hp](Entity entity) {
                 auto& hp = ecs.get_component<HPComponent>(entity);
                 hp.max_hp += max_hp;
             };
         } else if (name == "max_speed") {
-            auto [max_speed] = args_tuple;
-            return [max_speed](Entity entity) {
+            int max_speed = std::get<0>(args_tuple);
+            int seconds = -1;
+            if constexpr (sizeof...(args) == 2) {
+                seconds = std::get<1>(args_tuple);
+            }
+            return [max_speed, seconds](Entity entity) {
                 auto& velocity = ecs.get_component<VelocityComponent>(entity);
                 velocity.max_speed += max_speed;
+                if (seconds != -1) {
+                    timer_resource.add(config_resource.fps * seconds, 1, [entity, max_speed]() {
+                        auto& velocity = ecs.get_component<VelocityComponent>(entity);
+                        velocity.max_speed -= max_speed;
+                    });
+                }
             };
         } else if (name == "atk_ratio") {
-            auto [atk_ratio] = args_tuple;
+            int atk_ratio = std::get<0>(args_tuple);
             return [atk_ratio](Entity entity) {
                 auto& perk = ecs.get_component<PerkComponent>(entity);
                 perk.atk_ratio += atk_ratio;
             };
         } else if (name == "shoot_speed_ratio") {
-            auto [shoot_speed_ratio] = args_tuple;
+            int shoot_speed_ratio = std::get<0>(args_tuple);
             return [shoot_speed_ratio](Entity entity) {
                 auto& perk = ecs.get_component<PerkComponent>(entity);
                 perk.shoot_speed_ratio += shoot_speed_ratio;
             };
         } else if (name == "reload_speed_ratio") {
-            auto [reload_speed_ratio] = args_tuple;
+            int reload_speed_ratio = std::get<0>(args_tuple);
             return [reload_speed_ratio](Entity entity) {
                 auto& perk = ecs.get_component<PerkComponent>(entity);
                 perk.reload_speed_ratio += reload_speed_ratio;
             };
         } else if (name == "accuracy_ratio") {
-            auto [accuracy_ratio] = args_tuple;
+            int accuracy_ratio = std::get<0>(args_tuple);
             return [accuracy_ratio](Entity entity) {
                 auto& perk = ecs.get_component<PerkComponent>(entity);
                 perk.accuracy_ratio += accuracy_ratio;
             };
         } else if (name == "pick_range") {
-            auto [pick_range] = args_tuple;
+            int pick_range = std::get<0>(args_tuple);
             return [pick_range](Entity entity) {
                 auto& perk = ecs.get_component<PerkComponent>(entity);
                 perk.pick_range += pick_range;
             };
         } else if (name == "passwall") {
-            auto [seconds] = args_tuple;
+            int seconds = std::get<0>(args_tuple);
             return [seconds](Entity entity) {
                 if (!ecs.has_components<ColliderComponent>()) {
                     return;
