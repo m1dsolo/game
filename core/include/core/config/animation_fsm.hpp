2644f357a2e94e35aec68ab7c07e54e5b4f02fa3
m1dsolo <yx1053532442@gmail.com>
2025-10-03 11:18:58 +0800

    feat: AnimationFSM support from and to vector type

--- core/include/core/config/animation_fsm.hpp
+++ core/include/core/config/animation_fsm.hpp
@@ -2,13 +2,14 @@
 
 #include <string>
 #include <vector>
+#include <variant>
 
 namespace core {
 
 struct TransitionConfig {
-    std::string from;
-    std::string to;
-    std::vector<std::string> conditions;
+    std::variant<std::string, std::vector<std::string>> from;
+    std::variant<std::string, std::vector<std::string>> to;
+    std::variant<std::string, std::vector<std::string>> conditions;
 };
 
 struct AnimationFSMConfig {
--- core/src/manager/animation_fsm.cpp
+++ core/src/manager/animation_fsm.cpp
@@ -1,5 +1,6 @@
 #include <core/manager/animation_fsm.hpp>
 #include <core/config/animation_fsm.hpp>
+#include <core/util/utils.hpp>
 #include <core/animation_fsm.hpp>
 
 #include <rfl/json.hpp>
@@ -16,8 +17,16 @@ AnimationFSMManager::AnimationFSMManager() {
                 std::cout << "[begin load animation fsm...]" << entry.path() << std::endl;
                 const auto [name, transitions] = rfl::json::read<AnimationFSMConfig>(file).value();
                 AnimationFSM fsm{name};
-                for (const auto& [from, to, conditions] : transitions) {
-                    fsm.transitions[std::move(from)].emplace_back(std::move(to), std::move(conditions));
+                for (const auto& [from, to, condition] : transitions) {
+                    auto froms = Utils::variant_to_strings(from);
+                    auto tos = Utils::variant_to_strings(to);
+                    auto conditions = Utils::variant_to_strings(condition);
+                    // fsm.transitions[std::move(from)].emplace_back(std::move(to), std::move(conditions));
+                    for (const auto& f : froms) {
+                        for (const auto& t : tos) {
+                            fsm.transitions[f].emplace_back(t, conditions);
+                        }
+                    }
                 }
                 set(name, std::move(fsm));
                 std::cout << "[end load animation fsm...]" << std::endl;
